ed4a8f77e043b9c7b51f6c45d80aff6d9ce1c43d
chunkerchunker <chunkerchunker@gmail.com>
2025-09-29 23:37:04 -0700

    c version of auto line weight search

--- stringart_core.h
+++ stringart_core.h
@@ -1,66 +1,88 @@
 #ifndef STRINGART_CORE_H
 #define STRINGART_CORE_H
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
 #include "stb_image.h"
 #include "stb_image_write.h"
 
 typedef struct {
-    float x;
-    float y;
+  float x;
+  float y;
 } Coord;
 
 typedef struct {
-    int pins;
-    int maxLines;
-    int targetSize;
-    int outputSize;
-    int lineWeight;
-    int outputWeight;
-    int minDistance;
+  int pins;
+  int maxLines;
+  int targetSize;
+  int outputSize;
+  int lineWeight;
+  int outputWeight;
+  int minDistance;
 } Config;
 
 typedef struct {
-    int* coords;
-    int size;
-    float* errors;
+  int* coords;
+  int size;
+  float* errors;
 } FastLineCache;
 
 typedef struct {
-    Config config;
-    int imgSize;
-    int imgSizeSq;
+  Config config;
+  int imgSize;
+  int imgSizeSq;
 
-    Coord* pinCoords;
-    float* sourceImage;
-    float* errorImage;
-    FastLineCache* lineCache;
+  Coord* pinCoords;
+  float* sourceImage;
+  float* errorImage;
+  FastLineCache* lineCache;
 
-    int* validPins;
-    int* validCounts;
+  int* validPins;
+  int* validCounts;
 } FastStringArtGenerator;
 
 // Core algorithm functions (shared between all versions)
 void initGenerator(FastStringArtGenerator* gen, Config* cfg);
 void freeGenerator(FastStringArtGenerator* gen);
-void simpleResize(unsigned char* src, int srcW, int srcH, int channels,
-                 unsigned char* dst, int dstW, int dstH);
-void processImageData(FastStringArtGenerator* gen, unsigned char* imageData,
-                     int width, int height, int channels);
+void simpleResize(unsigned char* src,
+                  int srcW,
+                  int srcH,
+                  int channels,
+                  unsigned char* dst,
+                  int dstW,
+                  int dstH);
+void processImageData(FastStringArtGenerator* gen,
+                      unsigned char* imageData,
+                      int width,
+                      int height,
+                      int channels);
 void calculatePinCoords(FastStringArtGenerator* gen);
 void precalculateAllPotentialLines(FastStringArtGenerator* gen);
 int* calculateLines(FastStringArtGenerator* gen, int* lineCount);
-unsigned char* generateOutputImage(FastStringArtGenerator* gen, int* lineSequence, int lineCount);
+int findOptimalLineWeight(FastStringArtGenerator* gen,
+                          unsigned char* imageData,
+                          int width,
+                          int height,
+                          int channels,
+                          int verbose);
+unsigned char* generateOutputImage(FastStringArtGenerator* gen,
+                                   int* lineSequence,
+                                   int lineCount);
 
 // Drawing functions
-void drawLineAlpha(unsigned char* img, int x0, int y0, int x1, int y1, int alpha, int size);
+void drawLineAlpha(unsigned char* img,
+                   int x0,
+                   int y0,
+                   int x1,
+                   int y1,
+                   int alpha,
+                   int size);
 void drawFilledCircle(unsigned char* img, int cx, int cy, int r, int size);
 
 // Drawing functions (extra - not in core)
 void drawCircle(unsigned char* img, int cx, int cy, int r, int size);
 
-#endif // STRINGART_CORE_H
+#endif  // STRINGART_CORE_H
--- stringart_fast.c
+++ stringart_fast.c
@@ -1,121 +1,139 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stringart_core.h"
 #include <time.h>
+#include "stringart_core.h"
 
 typedef struct {
-    char inputFile[256];
-    char outputFile[256];
+  char inputFile[256];
+  char outputFile[256];
+  int autoWeight;
 } FileConfig;
 
-void loadAndProcessImage(FastStringArtGenerator* gen, const char* inputFile) {
-    int width, height, channels;
-    unsigned char* img = stbi_load(inputFile, &width, &height, &channels, 0);
+void generateOutputImageWithFile(FastStringArtGenerator* gen,
+                                 int* lineSequence,
+                                 int lineCount,
+                                 const char* outputFile) {
+  unsigned char* image = generateOutputImage(gen, lineSequence, lineCount);
 
-    if (!img) {
-        fprintf(stderr, "Error loading image: %s\n", inputFile);
-        exit(1);
-    }
+  int outSize = gen->config.outputSize;
+  int centerOut = outSize / 2;
+  int radiusOut = outSize / 2 - 1;
+  drawCircle(image, centerOut, centerOut, radiusOut, outSize);
 
-    processImageData(gen, img, width, height, channels);
-
-    if (width != gen->config.targetSize || height != gen->config.targetSize) {
-        printf("  Resized from %dx%d to %dx%d\n", width, height, gen->imgSize, gen->imgSize);
-    }
-
-    stbi_image_free(img);
-}
-
-void generateOutputImageWithFile(FastStringArtGenerator* gen, int* lineSequence, int lineCount, const char* outputFile) {
-    unsigned char* image = generateOutputImage(gen, lineSequence, lineCount);
-
-    int outSize = gen->config.outputSize;
-    int centerOut = outSize / 2;
-    int radiusOut = outSize / 2 - 1;
-    drawCircle(image, centerOut, centerOut, radiusOut, outSize);
-
-    stbi_write_png(outputFile, outSize, outSize, 4, image, outSize * 4);
-    free(image);
+  stbi_write_png(outputFile, outSize, outSize, 4, image, outSize * 4);
+  free(image);
 }
 
 int main(int argc, char* argv[]) {
-    Config config = {
-        .pins = 300,
-        .maxLines = 4000,
-        .targetSize = 500,
-        .outputSize = 0,
-        .lineWeight = 8,
-        .outputWeight = 0,
-        .minDistance = 30
-    };
+  Config config = {.pins = 288,
+                   .maxLines = 4000,
+                   .targetSize = 500,
+                   .outputSize = 0,
+                   .lineWeight = 8,
+                   .outputWeight = 0,
+                   .minDistance = 10};
 
-    FileConfig fileConfig = {
-        .inputFile = "",
-        .outputFile = "output.png"
-    };
+  FileConfig fileConfig = {
+      .inputFile = "", .outputFile = "output.png", .autoWeight = 0};
 
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-input") == 0 && i + 1 < argc) {
-            strcpy(fileConfig.inputFile, argv[++i]);
-        } else if (strcmp(argv[i], "-output") == 0 && i + 1 < argc) {
-            strcpy(fileConfig.outputFile, argv[++i]);
-        } else if (strcmp(argv[i], "-pins") == 0 && i + 1 < argc) {
-            config.pins = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "-lines") == 0 && i + 1 < argc) {
-            config.maxLines = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "-size") == 0 && i + 1 < argc) {
-            config.targetSize = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "-output-size") == 0 && i + 1 < argc) {
-            config.outputSize = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "-weight") == 0 && i + 1 < argc) {
-            config.lineWeight = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "-output-weight") == 0 && i + 1 < argc) {
-            config.outputWeight = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "-min-distance") == 0 && i + 1 < argc) {
-            config.minDistance = atoi(argv[++i]);
-        }
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-input") == 0 && i + 1 < argc) {
+      strcpy(fileConfig.inputFile, argv[++i]);
+    } else if (strcmp(argv[i], "-output") == 0 && i + 1 < argc) {
+      strcpy(fileConfig.outputFile, argv[++i]);
+    } else if (strcmp(argv[i], "-pins") == 0 && i + 1 < argc) {
+      config.pins = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-lines") == 0 && i + 1 < argc) {
+      config.maxLines = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-size") == 0 && i + 1 < argc) {
+      config.targetSize = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-output-size") == 0 && i + 1 < argc) {
+      config.outputSize = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-weight") == 0 && i + 1 < argc) {
+      config.lineWeight = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-output-weight") == 0 && i + 1 < argc) {
+      config.outputWeight = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-min-distance") == 0 && i + 1 < argc) {
+      config.minDistance = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-auto-weight") == 0) {
+      fileConfig.autoWeight = 1;
     }
+  }
 
-    if (config.outputSize == 0) {
-        config.outputSize = config.targetSize;
-    }
-    if (config.outputWeight == 0) {
-        config.outputWeight = config.lineWeight;
-    }
+  if (config.outputSize == 0) {
+    config.outputSize = config.targetSize;
+  }
+  if (config.outputWeight == 0) {
+    config.outputWeight = config.lineWeight;
+  }
 
-    if (strlen(fileConfig.inputFile) == 0) {
-        fprintf(stderr, "Please provide an input file using -input flag\n");
-        return 1;
-    }
+  if (strlen(fileConfig.inputFile) == 0) {
+    fprintf(stderr, "Please provide an input file using -input flag\n");
+    return 1;
+  }
 
-    printf("Processing %s...\n", fileConfig.inputFile);
-    printf("  Pins: %d, Max lines: %d, Processing size: %d, Output size: %d, Line weight: %d, Output weight: %d, Min distance: %d\n",
-           config.pins, config.maxLines, config.targetSize, config.outputSize,
-           config.lineWeight, config.outputWeight, config.minDistance);
+  printf("Processing %s...\n", fileConfig.inputFile);
+  if (fileConfig.autoWeight) {
+    printf("  Auto-weight mode enabled\n");
+    printf(
+        "  Pins: %d, Max lines: %d, Processing size: %d, Output size: %d, Line "
+        "weight: auto, Output weight: %d, Min distance: %d\n",
+        config.pins, config.maxLines, config.targetSize, config.outputSize,
+        config.outputWeight, config.minDistance);
+  } else {
+    printf(
+        "  Pins: %d, Max lines: %d, Processing size: %d, Output size: %d, Line "
+        "weight: %d, Output weight: %d, Min distance: %d\n",
+        config.pins, config.maxLines, config.targetSize, config.outputSize,
+        config.lineWeight, config.outputWeight, config.minDistance);
+  }
 
-    FastStringArtGenerator gen;
-    initGenerator(&gen, &config);
+  FastStringArtGenerator gen;
+  initGenerator(&gen, &config);
 
-    clock_t start = clock();
+  clock_t start = clock();
 
-    loadAndProcessImage(&gen, fileConfig.inputFile);
+  // Load image data for potential reuse in auto weight search
+  int width, height, channels;
+  unsigned char* img =
+      stbi_load(fileConfig.inputFile, &width, &height, &channels, 0);
+  if (!img) {
+    fprintf(stderr, "Error loading image: %s\n", fileConfig.inputFile);
+    exit(1);
+  }
+
+  if (fileConfig.autoWeight) {
+    // Find optimal weight using binary search
+    int optimalWeight =
+        findOptimalLineWeight(&gen, img, width, height, channels, 1);
+    config.lineWeight = optimalWeight;
+    config.outputWeight =
+        (config.outputWeight == 0) ? optimalWeight : config.outputWeight;
+    printf("  Found optimal weight: %d\n", optimalWeight);
+  } else {
+    // Normal processing
+    processImageData(&gen, img, width, height, channels);
     calculatePinCoords(&gen);
     precalculateAllPotentialLines(&gen);
+  }
 
-    printf("Calculating string art lines...\n");
-    int lineCount;
-    int* lineSequence = calculateLines(&gen, &lineCount);
+  printf("Calculating string art lines...\n");
+  int lineCount;
+  int* lineSequence = calculateLines(&gen, &lineCount);
 
-    clock_t end = clock();
-    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Processing took %.2f seconds\n", cpu_time_used);
+  stbi_image_free(img);
 
-    printf("Generating output image...\n");
-    generateOutputImageWithFile(&gen, lineSequence, lineCount, fileConfig.outputFile);
-    printf("Output saved to %s\n", fileConfig.outputFile);
+  clock_t end = clock();
+  double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("Processing took %.2f seconds\n", cpu_time_used);
 
-    free(lineSequence);
-    freeGenerator(&gen);
+  printf("Generating output image...\n");
+  generateOutputImageWithFile(&gen, lineSequence, lineCount,
+                              fileConfig.outputFile);
+  printf("Output saved to %s\n", fileConfig.outputFile);
 
-    return 0;
+  free(lineSequence);
+  freeGenerator(&gen);
+
+  return 0;
 }
